53d910d5e0924dc6219112a6a61950072f52f545
LeeKangHoo <protection0828@gmail.com>
2023-12-02 19:03:58 +0900

    ghost fix

--- Assets/Script/Ghost/GhostAI.cs
+++ Assets/Script/Ghost/GhostAI.cs
@@ -9,23 +9,35 @@ public class GhostAI : MonoBehaviour
     public float point1X = -7.19f;
     public float moveSpeed = 3f;
     public float targetY;
+    public int waypoint;
     public bool isMoving;
     void Start()
     {
         targetY = point1Y;
+        waypoint = 0;
+        isMoving = false;
     }
 
     void move(){
-        if(Mathf.Abs(transform.position.y - targetY)<0.1f)
+        if (waypoint == 0)
         {
-            if (isMoving)
+            targetY = point2Y;
+            if (point2Y - transform.position.y <0.1f)
+            {
+                waypoint = 1;
+                isMoving = true;
+            }
+        } 
+        if (waypoint == 1&& isMoving == true)
+            {
                 targetY = point1Y;
-            else
-                targetY = point2Y;
-            
-            isMoving = !isMoving;
-        }
-
+                if (transform.position.y - point1Y <0.1f)
+            {
+                waypoint = 0;
+                isMoving = false;
+            }
+            }
+        
         Vector3 targetPosition = new Vector3(transform.position.x, targetY,0f);
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
@@ -39,8 +51,8 @@ public class GhostAI : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("유령과 충돌했습니다!");
-            // 여기서 필요한 작업을 수행하세요.
-            // 예를 들어, 플레이어와의 충돌에 따른 게임 오버 처리 등
+            
         }
+        
     }
 }
--- Assets/Script/Ghost/GhostAI2.cs
+++ Assets/Script/Ghost/GhostAI2.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class GhostAI2 : MonoBehaviour
 {
-    
+    public float[] pointX = {3.27f,1.7f,0.85f,0.85f};
+    public float[] pointY = {2.17f,2.17f,1.18f,6.9f};
+    public float moveSpeed = 3f;
+    public float targetX;
+    public float targetY;
+    public bool isMoving;
 
+    public int waypoint;
     void Start()
     {
-        
+        isMoving = false;
+        waypoint = 0;
+        targetX = pointX[1];
+        targetY = pointY[1];
     }
 
+    void move(){
+        Debug.Log(transform.position);
+        
+
+        
+        Vector3 targetPosition = new Vector3(targetX, targetY,0f);
+        transform.position = Vector3.MoveTowards(transform.position,targetPosition, moveSpeed * Time.deltaTime);
+        
+    }
     
     void Update()
     {
-        
+        move();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("충돌!");
+            
+        }
     }
 }
