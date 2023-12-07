58acc3264f55aba82fd0693c397c80098d6ab6d5
LeeKangHoo <protection0828@gmail.com>
2023-12-07 14:49:34 +0900

    stair

--- Assets/Script/Camera/CameraController.cs
+++ Assets/Script/Camera/CameraController.cs
@@ -9,6 +9,7 @@ public class CameraController : MonoBehaviour
     public Camera mainCamera; 
     public GameObject FirstFloor;
     public GameObject SecondFloor;
+    public GameObject testObj;
     public bool stairState = false;
 
     void Start()
@@ -19,23 +20,32 @@ public class CameraController : MonoBehaviour
     //2�� �ö󰡴� �Լ�
     public void SecondStair()// 한 오브젝트에 , (아마 한 함수에도) 담아야함. 왜냐면 접촉하는게 두개라서. 하나의 오브젝트로 상호작용하게 하고 bool값으로 카메라 전환.
     {// 두개의 오브젝트를 써서 아래껄 밟으면 그건 bool값만 바뀌도록. 왜냐면 콜라이더 안에서 다시 방향전환을 하면 시점과 플레이어의 위치가 다름.
-        if (stairState == false)
-        {
-            StartCoroutine(SmoothMove(new Vector3(4, 2.5f, mainCamera.transform.position.z)));
-            SecondFloor.SetActive(false);
-            stairState = true;
-            FirstFloor.SetActive(true);
-        }
-    }
-    //1�� �������� �Լ�
-    public void FirstStair()
-    {   if(stairState)
+        /*if (stairState)
         {
             StartCoroutine(SmoothMove(new Vector3(-3.5f, -2, mainCamera.transform.position.z)));
             FirstFloor.SetActive(false);
             stairState = false;
             SecondFloor.SetActive(true);
-        }
+        }*/
+
+        
+            StartCoroutine(SmoothMove(new Vector3(-3.5f, -2, mainCamera.transform.position.z)));
+            //StartCoroutine(SmoothMove(new Vector3(4, 2.5f, mainCamera.transform.position.z)));
+            stairState = true;
+            FirstFloor.SetActive(true);
+            SecondFloor.SetActive(false);
+            
+        
+    }
+    //1�� �������� �Լ�
+    public void FirstStair()
+    {   
+            StartCoroutine(SmoothMove(new Vector3(4, 2.5f, mainCamera.transform.position.z)));
+            //StartCoroutine(SmoothMove(new Vector3(-3.5f, -2, mainCamera.transform.position.z)));
+            stairState = false;
+            SecondFloor.SetActive(true);
+            FirstFloor.SetActive(false);
+        
     }
 
     void Delay2()
